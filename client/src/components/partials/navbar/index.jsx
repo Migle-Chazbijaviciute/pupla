8185c92de5b74d35a23838f36ffb0635f961dc28
Miglė Chazbijavičiūtė <miglutech@gmail.com>
2022-01-25 15:41:01 +0200

    feat: menu toggler on smaller screens

--- client/src/components/partials/navbar/index.jsx
+++ client/src/components/partials/navbar/index.jsx
@@ -1,17 +1,47 @@
 import React from 'react';
 import {
-  AppBar, Box, Toolbar, IconButton, useTheme, styled, Link,
+  AppBar, Box, Toolbar, IconButton, useTheme, styled, Link, Menu, MenuItem,
 } from '@mui/material';
 import PermIdentityIcon from '@mui/icons-material/PermIdentity';
 import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import MenuIcon from '@mui/icons-material/Menu';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
   const theme = useTheme();
 
   const StyledButton = styled(IconButton)({
     textDecoration: 'none',
     color: theme.palette.primary.light,
   });
+
+  const StyledLink = styled(NavLink)({
+    color: theme.palette.primary.dark,
+    textTransform: 'uppercase',
+    paddingInline: 5,
+    textDecoration: 'none',
+    '&.active': {
+      textDecoration: 'underline',
+    },
+  });
+
+  const pages = [
+    { title: 'HOME', link: '/' },
+    { title: 'PRODUCTS', link: '/products' },
+    { title: 'INFORMATION', link: '/information' },
+    { title: 'CONTACT US', link: '/contactUs' },
+  ];
+
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <Box>
@@ -25,7 +55,31 @@ const Navbar = () => {
               width: 1 / 1,
             }}
           >
-            <Box sx={{ width: 80, flexGrow: 0 }} />
+            <Box sx={{ width: 70, flexGrow: 0 }}>
+              <IconButton
+                sx={{ display: { md: 'none' } }}
+                id="menu-button"
+                aria-controls={open ? 'menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+                color="secondary"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+              >
+                {pages.map(({ title, link }) => (
+                  <MenuItem onClick={handleClose} key={link}>
+                    <StyledLink to={link}>{title}</StyledLink>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
             <Link href="/">
               <Box
                 component="img"
