d31a9e6fe6130abb8c892dade7021a09ce7d95b1
Miglė Chazbijavičiūtė <miglutech@gmail.com>
2022-01-26 12:26:02 +0200

    feat: changed images and grid properties in products page

--- client/src/pages/products-page/products-grid.jsx
+++ client/src/pages/products-page/products-grid.jsx
@@ -6,6 +6,7 @@ import {
   useTheme,
   styled,
   Checkbox,
+  Link,
 } from '@mui/material';
 import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
 import Favorite from '@mui/icons-material/Favorite';
@@ -15,52 +16,52 @@ const ProductsGrid = () => {
 
   const itemData = [
     {
-      img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-      title: 'Breakfast',
+      img: 'https://scontent.fvno1-1.fna.fbcdn.net/v/t39.30808-6/262820488_4623068701113268_3692456574835045560_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=PxOG6n91ddwAX83GZg8&_nc_ht=scontent.fvno1-1.fna&oh=00_AT8I5Tg__tWB2EBKOK0d-aQ_cvOKziE5nYS2KFv3dLafoA&oe=61EBD601',
+      title: 'limited1',
     },
     {
-      img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-      title: 'Burger',
+      img: 'https://scontent.fvno1-1.fna.fbcdn.net/v/t39.30808-6/262572593_4623068711113267_5741378821227030688_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=XUcyFsOUGyMAX-bQSz6&_nc_ht=scontent.fvno1-1.fna&oh=00_AT81aWDpeGxd_6ACV8nnVQTfDGzAvZ1wldiX6Xa-IC8LaA&oe=61EB35DA',
+      title: 'limited2',
     },
     {
-      img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-      title: 'Camera',
+      img: 'https://scontent.fvno1-1.fna.fbcdn.net/v/t39.30808-6/261766297_4621107807976024_3560666404319759911_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=bvQLTCR5AkEAX9yqIr2&_nc_ht=scontent.fvno1-1.fna&oh=00_AT_-xUq39pPu4ppo4a5NcBV7_pvmZUxu4ruiK-sSCUX3Kw&oe=61EB22A8',
+      title: 'limited3',
     },
     {
-      img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-      title: 'Coffee',
+      img: 'https://scontent.fvno1-1.fna.fbcdn.net/v/t39.30808-6/262912268_4621107824642689_6315757410842768446_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=_ok9OJ5M_CEAX_vi2AO&_nc_ht=scontent.fvno1-1.fna&oh=00_AT_-rGtxOZoX_FjQP9Gt1xKyYzrFViQRviwaR4Yb89zAOQ&oe=61EB8E64',
+      title: 'limited4',
     },
     {
-      img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-      title: 'Hats',
+      img: 'https://scontent.fvno1-1.fna.fbcdn.net/v/t39.30808-6/262820488_4623068701113268_3692456574835045560_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=PxOG6n91ddwAX83GZg8&_nc_ht=scontent.fvno1-1.fna&oh=00_AT8I5Tg__tWB2EBKOK0d-aQ_cvOKziE5nYS2KFv3dLafoA&oe=61EBD601',
+      title: 'limited1',
     },
     {
-      img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-      title: 'Honey',
+      img: 'https://scontent.fvno1-1.fna.fbcdn.net/v/t39.30808-6/262572593_4623068711113267_5741378821227030688_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=XUcyFsOUGyMAX-bQSz6&_nc_ht=scontent.fvno1-1.fna&oh=00_AT81aWDpeGxd_6ACV8nnVQTfDGzAvZ1wldiX6Xa-IC8LaA&oe=61EB35DA',
+      title: 'limited2',
     },
     {
-      img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-      title: 'Basketball',
+      img: 'https://scontent.fvno1-1.fna.fbcdn.net/v/t39.30808-6/261766297_4621107807976024_3560666404319759911_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=bvQLTCR5AkEAX9yqIr2&_nc_ht=scontent.fvno1-1.fna&oh=00_AT_-xUq39pPu4ppo4a5NcBV7_pvmZUxu4ruiK-sSCUX3Kw&oe=61EB22A8',
+      title: 'limited3',
     },
     {
-      img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-      title: 'Fern',
+      img: 'https://scontent.fvno1-1.fna.fbcdn.net/v/t39.30808-6/262912268_4621107824642689_6315757410842768446_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=_ok9OJ5M_CEAX_vi2AO&_nc_ht=scontent.fvno1-1.fna&oh=00_AT_-rGtxOZoX_FjQP9Gt1xKyYzrFViQRviwaR4Yb89zAOQ&oe=61EB8E64',
+      title: 'limited4',
     },
     {
-      img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-      title: 'Mushrooms',
+      img: 'https://scontent.fvno1-1.fna.fbcdn.net/v/t39.30808-6/262820488_4623068701113268_3692456574835045560_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=PxOG6n91ddwAX83GZg8&_nc_ht=scontent.fvno1-1.fna&oh=00_AT8I5Tg__tWB2EBKOK0d-aQ_cvOKziE5nYS2KFv3dLafoA&oe=61EBD601',
+      title: 'limited1',
     },
     {
-      img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-      title: 'Tomato basil',
+      img: 'https://scontent.fvno1-1.fna.fbcdn.net/v/t39.30808-6/262572593_4623068711113267_5741378821227030688_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=XUcyFsOUGyMAX-bQSz6&_nc_ht=scontent.fvno1-1.fna&oh=00_AT81aWDpeGxd_6ACV8nnVQTfDGzAvZ1wldiX6Xa-IC8LaA&oe=61EB35DA',
+      title: 'limited2',
     },
     {
-      img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-      title: 'Sea star',
+      img: 'https://scontent.fvno1-1.fna.fbcdn.net/v/t39.30808-6/261766297_4621107807976024_3560666404319759911_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=bvQLTCR5AkEAX9yqIr2&_nc_ht=scontent.fvno1-1.fna&oh=00_AT_-xUq39pPu4ppo4a5NcBV7_pvmZUxu4ruiK-sSCUX3Kw&oe=61EB22A8',
+      title: 'limited3',
     },
     {
-      img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-      title: 'Bike',
+      img: 'https://scontent.fvno1-1.fna.fbcdn.net/v/t39.30808-6/262912268_4621107824642689_6315757410842768446_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=_ok9OJ5M_CEAX_vi2AO&_nc_ht=scontent.fvno1-1.fna&oh=00_AT_-rGtxOZoX_FjQP9Gt1xKyYzrFViQRviwaR4Yb89zAOQ&oe=61EB8E64',
+      title: 'limited4',
     },
   ];
 
@@ -85,14 +86,16 @@ const ProductsGrid = () => {
   return (
     <StyledGridContainer container maxWidth="90%">
       {itemData.map(({ img, title }) => (
-        <Grid item key={uuidv4()} xs={12} md={6} lg={4} xl={3} sx={{ position: 'relative' }}>
-          <Box
-            src={`${img}?w=164&h=164&fit=crop&auto=format`}
-            component="img"
-            width="100%"
-            alt={title}
-            loading="lazy"
-          />
+        <Grid item key={uuidv4()} xs={12} sm={6} md={4} xl={3} sx={{ position: 'relative' }}>
+          <Link href="/product/1">
+            <Box
+              src={img}
+              component="img"
+              width="100%"
+              alt={title}
+              loading="lazy"
+            />
+          </Link>
           <Checkbox
             icon={<FavoriteBorder />}
             checkedIcon={<Favorite />}
