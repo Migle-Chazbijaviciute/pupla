a4e8ca97cf7655ef69a9476ac4656c5e521563b5
Miglė Chazbijavičiūtė <miglutech@gmail.com>
2022-01-26 23:01:08 +0200

    feat: created separate single product card style for products grid

---
+++ client/src/pages/products-page/product-card.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import {
+  Box,
+  Grid,
+  useTheme,
+  styled,
+  Checkbox,
+  Link,
+  Typography,
+} from '@mui/material';
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+import Favorite from '@mui/icons-material/Favorite';
+
+const ProductCard = ({ img, title, price }) => {
+  const theme = useTheme();
+
+  const StyledInfo = styled(Typography)({
+    color: theme.palette.primary.dark,
+    textTransform: 'uppercase',
+    fontSize: '1.2rem',
+  });
+
+  return (
+    <Grid item key={uuidv4()} xs={12} sm={6} md={4} xl={3} sx={{ position: 'relative' }}>
+      <Link href="/product/1">
+        <Box
+          src={img}
+          component="img"
+          width="100%"
+          alt={title}
+          loading="lazy"
+          height="90%"
+        />
+      </Link>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', mx: 4 }}>
+        <StyledInfo>{title}</StyledInfo>
+        <StyledInfo sx={{ fontWeight: 550 }}>
+          {price}
+          €
+        </StyledInfo>
+      </Box>
+      <Checkbox
+        icon={<FavoriteBorder />}
+        checkedIcon={<Favorite />}
+        sx={{
+          position: 'absolute', right: 10, top: 10,
+        }}
+        size="medium"
+      />
+    </Grid>
+  );
+};
+
+export default ProductCard;
--- client/src/pages/products-page/products-grid.jsx
+++ client/src/pages/products-page/products-grid.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 import {
-  Box,
   Grid,
   useTheme,
   styled,
-  Checkbox,
-  Link,
 } from '@mui/material';
-import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
-import Favorite from '@mui/icons-material/Favorite';
+// import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+// import Favorite from '@mui/icons-material/Favorite';
+import ProductCard from './product-card';
 
 const ProductsGrid = () => {
   const theme = useTheme();
@@ -18,52 +16,70 @@ const ProductsGrid = () => {
     {
       img: 'https://scontent.fvno1-1.fna.fbcdn.net/v/t39.30808-6/262820488_4623068701113268_3692456574835045560_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=PxOG6n91ddwAX83GZg8&_nc_ht=scontent.fvno1-1.fna&oh=00_AT8I5Tg__tWB2EBKOK0d-aQ_cvOKziE5nYS2KFv3dLafoA&oe=61EBD601',
       title: 'limited1',
+      price: 50,
     },
     {
       img: 'https://scontent.fvno1-1.fna.fbcdn.net/v/t39.30808-6/262572593_4623068711113267_5741378821227030688_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=XUcyFsOUGyMAX-bQSz6&_nc_ht=scontent.fvno1-1.fna&oh=00_AT81aWDpeGxd_6ACV8nnVQTfDGzAvZ1wldiX6Xa-IC8LaA&oe=61EB35DA',
       title: 'limited2',
+      price: 50,
     },
     {
       img: 'https://scontent.fvno1-1.fna.fbcdn.net/v/t39.30808-6/261766297_4621107807976024_3560666404319759911_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=bvQLTCR5AkEAX9yqIr2&_nc_ht=scontent.fvno1-1.fna&oh=00_AT_-xUq39pPu4ppo4a5NcBV7_pvmZUxu4ruiK-sSCUX3Kw&oe=61EB22A8',
       title: 'limited3',
+      price: 50,
     },
     {
       img: 'https://scontent.fvno1-1.fna.fbcdn.net/v/t39.30808-6/262912268_4621107824642689_6315757410842768446_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=_ok9OJ5M_CEAX_vi2AO&_nc_ht=scontent.fvno1-1.fna&oh=00_AT_-rGtxOZoX_FjQP9Gt1xKyYzrFViQRviwaR4Yb89zAOQ&oe=61EB8E64',
       title: 'limited4',
+      price: 50,
     },
     {
       img: 'https://scontent.fvno1-1.fna.fbcdn.net/v/t39.30808-6/262820488_4623068701113268_3692456574835045560_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=PxOG6n91ddwAX83GZg8&_nc_ht=scontent.fvno1-1.fna&oh=00_AT8I5Tg__tWB2EBKOK0d-aQ_cvOKziE5nYS2KFv3dLafoA&oe=61EBD601',
       title: 'limited1',
+      price: 50,
     },
     {
       img: 'https://scontent.fvno1-1.fna.fbcdn.net/v/t39.30808-6/262572593_4623068711113267_5741378821227030688_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=XUcyFsOUGyMAX-bQSz6&_nc_ht=scontent.fvno1-1.fna&oh=00_AT81aWDpeGxd_6ACV8nnVQTfDGzAvZ1wldiX6Xa-IC8LaA&oe=61EB35DA',
       title: 'limited2',
+      price: 50,
     },
     {
       img: 'https://scontent.fvno1-1.fna.fbcdn.net/v/t39.30808-6/261766297_4621107807976024_3560666404319759911_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=bvQLTCR5AkEAX9yqIr2&_nc_ht=scontent.fvno1-1.fna&oh=00_AT_-xUq39pPu4ppo4a5NcBV7_pvmZUxu4ruiK-sSCUX3Kw&oe=61EB22A8',
       title: 'limited3',
+      price: 50,
     },
     {
       img: 'https://scontent.fvno1-1.fna.fbcdn.net/v/t39.30808-6/262912268_4621107824642689_6315757410842768446_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=_ok9OJ5M_CEAX_vi2AO&_nc_ht=scontent.fvno1-1.fna&oh=00_AT_-rGtxOZoX_FjQP9Gt1xKyYzrFViQRviwaR4Yb89zAOQ&oe=61EB8E64',
       title: 'limited4',
+      price: 50,
     },
     {
       img: 'https://scontent.fvno1-1.fna.fbcdn.net/v/t39.30808-6/262820488_4623068701113268_3692456574835045560_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=PxOG6n91ddwAX83GZg8&_nc_ht=scontent.fvno1-1.fna&oh=00_AT8I5Tg__tWB2EBKOK0d-aQ_cvOKziE5nYS2KFv3dLafoA&oe=61EBD601',
       title: 'limited1',
+      price: 50,
     },
     {
       img: 'https://scontent.fvno1-1.fna.fbcdn.net/v/t39.30808-6/262572593_4623068711113267_5741378821227030688_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=XUcyFsOUGyMAX-bQSz6&_nc_ht=scontent.fvno1-1.fna&oh=00_AT81aWDpeGxd_6ACV8nnVQTfDGzAvZ1wldiX6Xa-IC8LaA&oe=61EB35DA',
       title: 'limited2',
+      price: 50,
     },
     {
       img: 'https://scontent.fvno1-1.fna.fbcdn.net/v/t39.30808-6/261766297_4621107807976024_3560666404319759911_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=bvQLTCR5AkEAX9yqIr2&_nc_ht=scontent.fvno1-1.fna&oh=00_AT_-xUq39pPu4ppo4a5NcBV7_pvmZUxu4ruiK-sSCUX3Kw&oe=61EB22A8',
       title: 'limited3',
+      price: 50,
     },
     {
       img: 'https://scontent.fvno1-1.fna.fbcdn.net/v/t39.30808-6/262912268_4621107824642689_6315757410842768446_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=_ok9OJ5M_CEAX_vi2AO&_nc_ht=scontent.fvno1-1.fna&oh=00_AT_-rGtxOZoX_FjQP9Gt1xKyYzrFViQRviwaR4Yb89zAOQ&oe=61EB8E64',
       title: 'limited4',
+      price: 50,
     },
   ];
+
+  // const StyledInfo = styled(Typography)({
+  //   color: theme.palette.primary.dark,
+  //   textTransform: 'uppercase',
+  //   fontSize: '1.2rem',
+  // });
 
   const StyledGridContainer = styled(Grid)({
     backgroundColor: theme.palette.secondary.main,
@@ -85,26 +101,8 @@ const ProductsGrid = () => {
 
   return (
     <StyledGridContainer container maxWidth="90%">
-      {itemData.map(({ img, title }) => (
-        <Grid item key={uuidv4()} xs={12} sm={6} md={4} xl={3} sx={{ position: 'relative' }}>
-          <Link href="/product/1">
-            <Box
-              src={img}
-              component="img"
-              width="100%"
-              alt={title}
-              loading="lazy"
-            />
-          </Link>
-          <Checkbox
-            icon={<FavoriteBorder />}
-            checkedIcon={<Favorite />}
-            sx={{
-              position: 'absolute', right: 0, top: 0,
-            }}
-            size="large"
-          />
-        </Grid>
+      {itemData.map((item) => (
+        <ProductCard {...item} />
       ))}
     </StyledGridContainer>
   );
