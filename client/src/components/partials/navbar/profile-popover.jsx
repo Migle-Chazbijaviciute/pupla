e27d53e63affb3c03b48c446a2e27704957ec4f0
Miglė Chazbijavičiūtė <miglutech@gmail.com>
2022-02-06 21:35:38 +0200

    feat: changed popover to conditional popover depending on loggedIn value

--- client/src/components/partials/navbar/profile-popover.jsx
+++ client/src/components/partials/navbar/profile-popover.jsx
@@ -8,21 +8,24 @@ import {
   styled,
   useTheme,
 } from '@mui/material';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import routes from '../../../routing/routes';
+import AuthService from '../../../services/auth-service';
+import { authSelector } from '../../../store/auth';
 
 const ProfilePopover = () => {
+  const auth = useSelector(authSelector);
   const theme = useTheme();
   const anchorRef = useRef();
   const [menuOpen, setMenuOpen] = useState(false);
   const handleOpenMenu = () => setMenuOpen(true);
   const handleCloseMenu = () => setMenuOpen(false);
-
-  const userPages = [
-    { title: 'PROFILE', link: '/profile' },
-    { title: 'LOGIN', link: '/login' },
-    { title: 'REGISTER', link: '/register' },
-  ];
+  const handleLogout = () => {
+    handleCloseMenu();
+    AuthService.logout();
+  };
 
   const StyledLink = styled(NavLink)({
     color: theme.palette.primary.dark,
@@ -39,27 +42,57 @@ const ProfilePopover = () => {
       <IconButton onClick={handleOpenMenu} ref={anchorRef} color="secondary">
         <PermIdentityIcon />
       </IconButton>
-      <Menu
-        open={menuOpen}
-        onClose={handleCloseMenu}
-        anchorEl={anchorRef.current}
-        anchorOrigin={{
-          horizontal: 'right',
-          vertical: 'bottom',
-        }}
-        transformOrigin={{
-          horizontal: 'right',
-          vertical: 'top',
-        }}
-      >
-        {userPages.map(({ title, link }) => (
-          <MenuItem onClick={handleCloseMenu} key={link}>
-            <StyledLink to={link}>
-              <Typography textAlign="center">{title}</Typography>
-            </StyledLink>
-          </MenuItem>
-        ))}
-      </Menu>
+      {auth.loggedIn
+        ? (
+          <Menu
+            open={menuOpen}
+            onClose={handleCloseMenu}
+            anchorEl={anchorRef.current}
+            anchorOrigin={{
+              horizontal: 'right',
+              vertical: 'bottom',
+            }}
+            transformOrigin={{
+              horizontal: 'right',
+              vertical: 'top',
+            }}
+          >
+            <MenuItem onClick={handleCloseMenu}>
+              <StyledLink to={routes.ProfilePage}>
+                <Typography textAlign="center">PROFILE</Typography>
+              </StyledLink>
+            </MenuItem>
+            <MenuItem onClick={handleLogout}>
+              <Typography textAlign="center">LOGOUT</Typography>
+            </MenuItem>
+          </Menu>
+        )
+        : (
+          <Menu
+            open={menuOpen}
+            onClose={handleCloseMenu}
+            anchorEl={anchorRef.current}
+            anchorOrigin={{
+              horizontal: 'right',
+              vertical: 'bottom',
+            }}
+            transformOrigin={{
+              horizontal: 'right',
+              vertical: 'top',
+            }}
+          >
+            <MenuItem onClick={handleCloseMenu}>
+              <StyledLink to={routes.LoginPage}>
+                <Typography textAlign="center">LOGIN</Typography>
+              </StyledLink>
+            </MenuItem>
+            <MenuItem onClick={handleCloseMenu}>
+              <StyledLink to={routes.RegisterPage}>
+                <Typography textAlign="center">REGISTER</Typography>
+              </StyledLink>
+            </MenuItem>
+          </Menu>
+        )}
     </Box>
   );
 };
