eef225bac12554eaff6dab029981678c16d07677
Miglė Chazbijavičiūtė <miglutech@gmail.com>
2022-03-03 17:33:55 +0200

    feat: enabled additional props for card component

--- client/src/pages/products-page/product-card.tsx
+++ client/src/pages/products-page/product-card.tsx
@@ -6,6 +6,8 @@ import {
   Button,
   IconButton,
   Checkbox,
+  SxProps,
+  Theme,
 } from '@mui/material';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
@@ -20,23 +22,35 @@ type ProductCardProps = {
   price: string;
   shouldAddButton?: boolean;
   deleteIcon?: boolean;
+  sx?: SxProps<Theme>;
 };
 
 const ProductCard: React.FC<ProductCardProps> = ({
-  id, images, label, price, shouldAddButton, deleteIcon,
+  id,
+  images,
+  label,
+  price,
+  shouldAddButton,
+  deleteIcon,
+  sx,
 }) => {
   if (images === undefined) return null;
+
   return (
-    <Grid item key={id} xs={12} sm={6} md={4} xl={3} position="relative">
+    <Grid sx={{ width: { xs: '100%', lg: '80%' } }} position="relative" item key={id} xs={12} sm={6} md={4} xl={3}>
       <Link href={`/product/${id}`}>
         <Box
           src={images[0].src}
           component="img"
           sx={{
             width: '100%',
-            height: { xs: 650 },
+            height: { xs: 400, sm: 500, md: 650 },
             objectFit: 'cover',
-            paddingBlock: 20,
+            paddingBlock: 10,
+            '&:hover': {
+              cursor: 'pointer',
+            },
+            ...sx,
           }}
           alt={label}
           loading="lazy"
@@ -64,9 +78,9 @@ const ProductCard: React.FC<ProductCardProps> = ({
           ? (
             <IconButton
               sx={{
-                position: 'absolute', right: 10, top: 10,
+                position: 'absolute', right: 10, top: 30, padding: 0,
               }}
-              size="medium"
+              size="large"
             >
               <DeleteOutlineIcon />
             </IconButton>
@@ -76,7 +90,7 @@ const ProductCard: React.FC<ProductCardProps> = ({
               icon={<FavoriteBorder />}
               checkedIcon={<Favorite />}
               sx={{
-                position: 'absolute', right: 10, top: 40,
+                position: 'absolute', right: 10, top: 30,
               }}
               size="medium"
             />
