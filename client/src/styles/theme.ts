01721477920f18aa4934e7f3632575a45f3ec398
Miglė Chazbijavičiūtė <miglutech@gmail.com>
2022-03-04 09:10:56 +0200

    feat: changed font style

--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -14,18 +14,7 @@ const theme = createTheme({
     },
   },
   typography: {
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
+    fontFamily: '\'Montserrat\', sans-serif',
   },
   mixins: {
     toolbar: {
